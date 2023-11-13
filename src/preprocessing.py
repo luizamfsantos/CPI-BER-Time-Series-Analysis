a87dfd0ffc6bf6e378194e82b983d6e1590226b1
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-13 16:07:21 -0300

    load data and update column types

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,28 +1,38 @@
+import pandas as pd
+
 def load_data(file_path):
     """
-    Load data from a given file path.
+    Load csv data from a given path.
 
     Parameters:
     - file_path (str): Path to the data file.
 
     Returns:
     - pd.DataFrame: Loaded data.
+        Correctly formatted data should have two columns:
+        - date: Date of the observation
+        - value: Value of the observation
     """
-    # Implementation goes here
-    pass
+    data = pd.read_csv(file_path)
+    data = update_dtypes(data)
+    return data
 
-def remove_missing_days(data):
-    """
-    Remove missing days from the dataset.
+def update_dtypes(data):
+    '''
+    Update the data types of the columns.
+    Each dataset should have a data column and a float column.
 
     Parameters:
     - data (pd.DataFrame): Input data.
 
     Returns:
-    - pd.DataFrame: Data without missing days.
-    """
-    # Implementation goes here
-    pass
+    - pd.DataFrame: Updated data.
+    '''
+    data.columns = ['date', 'value']
+    data['date'] = pd.to_datetime(data['date'])
+    data['value'] = pd.to_numeric(data['value'])
+    return data
+
 
 def calculate_yearly_average(data):
     """
@@ -51,3 +61,12 @@ def subset_data(data, start_date, end_date):
     """
     # Implementation goes here
     pass
+
+if __name__ == '__main__':
+    # Load data
+    cpi_path = 'data/raw/CPI.csv'
+    ber_path = 'data/raw/T10YIE.csv'
+    cpi = load_data(cpi_path)
+    ber = load_data(ber_path)
+    print(cpi.head())
+    print(ber.head())
