d2f31acc57f0dc73e41cb46651af45a11d029365
luizamfsantos <luiza.marques_@hotmail.com>
2023-11-20 09:20:17 -0300

    modify subtract_ar function to take pd.series rather than entire dataframe

--- src/time_series.py
+++ src/time_series.py
@@ -53,7 +53,7 @@ def subtract_ar(data, ar_params):
     - pd.DataFrame: DataFrame with original 't' and residuals from the AR model.
     '''
     # Extract the time series values from the 't' column
-    time_series = data['residuals']
+    time_series = data
 
     # Calculate the residuals using the AR model
     residuals = np.zeros_like(time_series)
@@ -62,39 +62,9 @@ def subtract_ar(data, ar_params):
         for j in range(len(ar_params)):
             residuals[i] += ar_params[j] * time_series[i - j - 1]
 
-    # Create a DataFrame with the original 't' values and residuals
-    result_data = pd.DataFrame({
-        't': data['t'],
-        'residuals': residuals
-    })
-
-    return result_data
+    return residuals
 
 
 
 if __name__ == '__main__':
-    # Load data
-    from preprocessing import load_indexed_data
-    path_cpi_train_residuals = 'data/processed/cpi_train_residuals.csv'
-    cpi_train_residuals = load_indexed_data(path_cpi_train_residuals)
-    
-    # Plot ACF and PACF
-    # plot_acf_pacf(cpi_train_residuals['residuals'])
-
-    # Fit AR(2)
-    order = (2, 0)
-    model = fit_arma_model(cpi_train_residuals['residuals'], order)
-
-    # Return model summary
-    print(model.summary())
-
-    # Print model parameters
-    print('Model parameters:', model.params)
-
-    # Decompose model parameters
-    phi_1 = model.params[1]
-    phi_2 = model.params[2]
-
-    # Save model parameters
-    model_parameters_df = pd.DataFrame({'phi_1': [phi_1], 'phi_2': [phi_2]})
-    model_parameters_df.to_csv('data/processed/arma_model_params.csv', index=False)
+    pass
